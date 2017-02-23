3285399d02d1a013d6825c3149f73edcff5b15cd
p15216326 <p15216326@GWHXX581WIN012W>
2017-02-23 11:17:28 +0000

    Added all work

--- ClassLibrary/clsBooking.cs
+++ ClassLibrary/clsBooking.cs
@@ -3,17 +3,142 @@
 namespace ClassLibrary
 {
     public class clsBooking
-    {
-        public DateTime BookingDate { get; set; }
-        public int BookingID { get; set; }
-        public DateTime CheckInDate { get; set; }
-        public DateTime CheckOutDate { get; set; }
-        public int CustomerID { get; set; }
-        public int HotelID { get; set; }
-        public int NumOfNights { get; set; }
-        public int RoomNo { get; set; }
+    { //private data member for the BookingID Property
+        private Int32 aBookingId;
+        //private data member for the HotelID property
+        private Int32 aHotelId;
+        //private data member for the customerID property
+        private Int32 aCustomerId;
+        //private data member for the RoomId property
+        private Int32 aRoomNo;
+        //private data member for the BookingDate property
+        private DateTime aBookingDate;
+        //private data member for the checkInDate property
+        private DateTime aCheckInDate;
+        //private data member for the checkOutDate
+        private DateTime aCheckOutDate;
+        //private data member for the NumOfNights property
+        private Int32 aNumOfNights;
 
-        
+        //public property for BookingId
+        public int BookingID
+        {
+            get
+            {
+                //retrun the private data
+                return aBookingId;
+            }
+            set
+            {
+                //set the private data
+                aBookingId = value;
+            }
+        }
+        //public property for BookingDate
+        public DateTime BookingDate
+        {
+            get
+            {
+                //retrun the private data
+                return aBookingDate;
+            }
+            set
+            {
+                //set the private data
+                aBookingDate = value;
+            }
+        }
+
+        //public property for CheckInDate
+        public DateTime CheckInDate
+        {
+            get
+            {
+                //retrun the private data
+                return aCheckInDate;
+            }
+            set
+            {
+                //set the private data
+                aCheckInDate = value;
+            }
+        }
+
+        //public property for checkOutDate
+        public DateTime CheckOutDate
+        {
+            get
+            {
+                //retrun the private data
+                return aCheckOutDate;
+            }
+            set
+            {
+                //set the private data
+                aCheckOutDate = value;
+            }
+        }
+
+        //public property for CustomerId
+        public int CustomerID
+        {
+            get
+            {
+                //retrun the private data
+                return aCustomerId;
+            }
+            set
+            {
+                //set the private data
+                aCustomerId = value;
+            }
+        }
+
+        //public property for HotelId
+        public int HotelID
+        {
+            get
+            {
+                //retrun the private data
+                return aHotelId;
+            }
+            set
+            {
+                //set the private data
+                aHotelId = value;
+            }
+        }
+        //public property for the NumOfNights
+        public int NumOfNights
+        {
+            get
+            {
+                //retrun the private data
+                return aNumOfNights;
+            }
+            set
+            {
+                //set the private data
+                aNumOfNights = value;
+            }
+        }
+
+        //public property for roomNo
+        public int RoomNo
+        {
+            get
+            {
+                //retrun the private data
+                return aRoomNo;
+            }
+            set
+            {
+                //set the private data
+                aRoomNo = value;
+            }
+        }
+
+
 
         public bool Valid(string numOfNights, string bookigDate, string checkInDate, string checkOutDate)
         {
@@ -107,7 +232,33 @@ namespace ClassLibrary
 
         public bool Find(int bookingID)
         {
-            return true;
+            //create an instance of data connection class
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the booking id to serch for
+            DB.AddParameter("BookingId", bookingID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblBooking_FilterByBookingId");
+            //if one record found
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                aBookingId = Convert.ToInt32(DB.DataTable.Rows[0]["BookingId"]);
+                aCustomerId = Convert.ToInt32(DB.DataTable.Rows[0]["CustomerId"]);
+                aHotelId = Convert.ToInt32(DB.DataTable.Rows[0]["HotelId"]);
+                aRoomNo = Convert.ToInt32(DB.DataTable.Rows[0]["RoomNo"]);
+                aBookingDate = Convert.ToDateTime(DB.DataTable.Rows[0]["BookingDate"]);
+                aCheckInDate = Convert.ToDateTime(DB.DataTable.Rows[0]["CheckInDate"]);
+                aCheckOutDate = Convert.ToDateTime(DB.DataTable.Rows[0]["CheckOutDate"]);
+                aNumOfNights = Convert.ToInt32(DB.DataTable.Rows[0]["NumOfNights"]);
+                return true;
+
+            }
+            //if no record was found
+            else
+            {
+                //return false indication a problem
+                return false;
+            }
         }
     }
 }
